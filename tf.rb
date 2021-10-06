d3bf2b4075a3b808e32f7848beab1eeb06dd7c05
iuri aranda <iuri@skyscrapers.eu>
2021-10-06 18:48:16 +0200

    Update tf brew formula

--- tf.rb
+++ tf.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class BrewTf < Formula
+class Tf < Formula
   desc "TF script"
   homepage "https://github.com/skyscrapers/brew-tools"
   url "https://github.com/skyscrapers/brew-tools/archive/refs/tags/v1.0.0.tar.gz"
